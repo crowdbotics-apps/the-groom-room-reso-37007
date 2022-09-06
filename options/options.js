c00f1ba25077f8e122a069bd952cd09d675ef997
Crowdbotics <team@crowdbotics.com>
2022-09-06 19:30:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "the_groom_room_reso_37007",
+  url: "https://the_groom_room_reso_37007.botics.co",
+  api: "https://the_groom_room_reso_37007.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
